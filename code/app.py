95cccc039fd570122532d780c25d8d3c8e38cb73
Sarthak Saxena <saxenasarthak72@gmail.com>
2020-10-19 16:35:25 +0530

    progress

--- code/app.py
+++ code/app.py
@@ -1,17 +1,19 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_restful import Api
-from flask_jwt import JWT
+from flask_jwt_extended import JWTManager
 
 from db import db
 from resources.item import Item, ItemList
-from resources.user import UserRegister
+from resources.user import UserRegister, User, UserLogin, TokenRefresh, UserLogout
 from resources.store import Store, StoreList
-from security import authenticate, identity
-
+from blacklist import BLACKLIST
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['PROPAGATE_EXCEPTIONS'] = True
+app.config['JWT_BLACKLIST_ENABLED'] = True
+app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access', 'refresh']
 db.init_app(app)
 api = Api(app)
 app.secret_key = 'sarthak'
@@ -22,13 +24,70 @@ def create_tables():
     db.create_all()
 
 
-jwt = JWT(app, authenticate, identity)
+jwt = JWTManager(app)
+
+
+@jwt.user_claims_loader
+def add_claims_to_jwt(identity):
+    if identity == 1:
+        return {'is_admin': True}
+    return {'is_admin': False}
+
+
+@jwt.token_in_blacklist_loader
+def check_if_token_in_blacklist(decrypted_token):
+    return decrypted_token['jti'] in BLACKLIST
+
+
+@jwt.expired_token_loader
+def expired_token_callback():
+    return jsonify({
+        'description': 'The token has expired',
+        'error': 'expired_token'
+    }), 401
+
+
+@jwt.invalid_token_loader
+def invalid_token_callback(error):
+    return jsonify({
+        'message': 'Signature verification failed.',
+        'error': 'invalid_token'
+    }), 401
+
+
+@jwt.unauthorized_loader
+def missing_token_callback(error):
+    return jsonify({
+        'description': 'Token is missing',
+        'error': 'missing_token'
+    })
+
+
+@jwt.needs_fresh_token_loader
+def no_fresh_token_callback():
+    return jsonify({
+        'description': 'This token is not fresh',
+        'error': 'not_fresh_token'
+    })
+
+
+@jwt.revoked_token_loader
+def revoked_token_callback():
+    return jsonify({
+        'description': 'This token has been revoked',
+        'error': 'revoked_token'
+    })
+
 
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
 api.add_resource(Store, '/store/<string:name>')
 api.add_resource(StoreList, '/stores')
+api.add_resource(User, '/user/<int:user_id>')
+api.add_resource(UserLogin, '/login')
+api.add_resource(TokenRefresh, '/refresh')
+api.add_resource(UserLogout, '/logout')
 
 if __name__ == '__main__':
     app.run(port=5000)
